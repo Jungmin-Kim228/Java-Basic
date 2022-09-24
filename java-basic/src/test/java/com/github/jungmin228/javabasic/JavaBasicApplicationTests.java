a67517cdf8b861a5f1ee5564351f6f3f715be997
moomink <moomink228@gmail.com>
2022-09-24 12:54:36 +0900

    initialize project

---
+++ java-basic/src/test/java/com/github/jungmin228/javabasic/JavaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jungmin228.javabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
